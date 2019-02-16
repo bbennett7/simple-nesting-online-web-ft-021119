f8cb4d8bcdca02e02d9d1b4bcd2002df78f43e3b
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-16 01:30:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,8 +78,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
@@ -99,9 +99,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+  alans_new_info = "GUI"
+  
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -120,5 +122,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 end
